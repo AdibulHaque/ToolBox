cbff198c883cdedb17280cb53f7f46678eeeacc6
Adib <adib1690@gmail.com>
2025-04-03 19:20:10 +0600

    Initial commit

---
+++ Bulky.Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToolB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
